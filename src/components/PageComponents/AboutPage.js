3dfbb74f97413210fd9a4d9f089d32bf14212232
Your Name <121236744+Monsieur-Nobody@users.noreply.github.com>
2024-08-16 21:42:44 +0200

    16/08.2

--- src/components/PageComponents/AboutPage.js
+++ src/components/PageComponents/AboutPage.js
@@ -1,18 +1,13 @@
-import BannerAbout from '../ParentsComponents/BannerAbout';
-import AboutCollapse from '../ParentsComponents/AboutCollapse';
-
-
-
-
+import BannerAbout from "../ParentsComponents/BannerAbout";
+import AboutContent from "../ParentsComponents/AboutContent";
 
 const AboutPage = () => {
-    return (
-        <div className="Home_Page_Style">
-            <BannerAbout/>
-            <AboutCollapse/>
-        </div>
-    )
-}
-
+  return (
+    <div className="Home_Page_Style">
+      <BannerAbout />
+      <AboutContent />
+    </div>
+  );
+};
 
 export default AboutPage;
