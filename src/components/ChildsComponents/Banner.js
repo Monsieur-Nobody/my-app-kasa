1225e78f34e9bfed6c5124608eb2ac9ac826c836
Your Name <121236744+Monsieur-Nobody@users.noreply.github.com>
2024-09-08 22:09:26 +0200

    08/09v3

--- src/components/ChildsComponents/Banner.js
+++ src/components/ChildsComponents/Banner.js
@@ -1,27 +1,15 @@
+const Banner = ({
+  imageSrc,
+  text = "",
+  altText = "Banner Image",
+  bannerClass,
+}) => {
+  return (
+    <div className={`banner ${bannerClass}`}>
+      <img src={imageSrc} alt={altText} className="banner-image" />
+      {text && <div className="banner-text">{text}</div>}
+    </div>
+  );
+};
 
-import PropTypes from 'prop-types';
-
-const Banner = ({ imageSrc, text, altText, bannerClass }) => {
-    return (
-      <div className={`banner ${bannerClass}`}>
-        <img src={imageSrc} alt={altText} className="banner-image" />
-        {text && <div className="banner-text">{text}</div>}
-      </div>
-    );
-  };
-
-
-  Banner.propTypes = {
-    imageSrc: PropTypes.string.isRequired,
-    text: PropTypes.string,
-    altText: PropTypes.string,
-    bannerClass: PropTypes.string,
-  };
-  
-  Banner.defaultProps = {
-    text: '',
-    altText: 'Banner image',
-    bannerClass: '', 
-  };
-  
-  export default Banner;
+export default Banner;
