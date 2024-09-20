3237cabdfde89d9efb2066ae4533e2de3da0f75c
Your Name <121236744+Monsieur-Nobody@users.noreply.github.com>
2024-09-20 13:27:15 +0200

    20/09

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,11 +1,26 @@
-import { Link } from 'react-router-dom';
+import { Link, useLocation } from "react-router-dom";
 
 const Nav = () => {
+  const location = useLocation();
   return (
     <nav className="Nav">
-      <ul className='Nav_Links'>
-        <li><Link to="/" className='HomeLink'>Accueil</Link></li>
-        <li><Link to="/about" className='AboutLink'>A Propos</Link></li>
+      <ul className="Nav_Links">
+        <li>
+          <Link
+            to="/"
+            className={location.pathname === "/" ? "active-link" : ""}
+          >
+            Accueil
+          </Link>
+        </li>
+        <li>
+          <Link
+            to="/about"
+            className={location.pathname === "/about" ? "active-link" : ""}
+          >
+            A Propos
+          </Link>
+        </li>
       </ul>
     </nav>
   );
